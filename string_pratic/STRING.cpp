0c2996cdfe9cbfff23ae1c0526010ce021ff5624
DucTamDinhUIT <ductam.astro@gmail.com>
2020-06-28 18:19:05 +0700

    Create STRING.cpp

---
+++ string_pratic/STRING.cpp
@@ -0,0 +1,39 @@
+
+#include "STRING.h"
+STRING::STRING()
+{
+
+};
+STRING::~STRING()
+{
+
+};
+STRING::STRING()
+{
+
+};
+CHAR STRING::getContent()
+{
+	CHAR temp;
+	cout << content;
+	return temp;
+};
+int STRING::expand()
+{
+	return 0;
+};
+int STRING::remove()
+{
+	return 0;
+};
+int STRING::input()
+{
+	return 0;
+};
+/*
+STRING STRING::(x)
+{
+	STRING temp;
+	return temp;
+};
+*/
