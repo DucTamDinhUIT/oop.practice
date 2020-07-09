33b40e2b5858b1a3eda84c43437f366e319b2afb
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-10 03:10:33 +0700

    Create Source.cpp

---
+++ OOP.nhommau/Source.cpp
@@ -0,0 +1,15 @@
+#include "CNhomMau.h"
+#include "CMauA.h"
+#include "CMauB.h"
+#include "CMauAB.h"
+#include "CMauO.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
