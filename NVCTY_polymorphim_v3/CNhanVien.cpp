e7b4c43ffc5ce4ec338d98b5e2bfade57269c584
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-10 03:09:15 +0700

    Create CNhanVien.cpp

---
+++ NVCTY_polymorphim_v3/CNhanVien.cpp
@@ -0,0 +1,37 @@
+#include "CNhanVien.h"
+
+
+CNhanVien::CNhanVien()
+{
+	this->stHoTen = "";
+	this->stDiaChi = "";
+	this->stNgaySinh = "";
+}
+
+CNhanVien::~CNhanVien()
+{
+}
+
+void CNhanVien::Nhap()
+{
+	cout << "nhap ho ten: ";
+	cin.ignore();
+	getline(cin, this->stHoTen);
+	cout << "nhap ngay sinh: ";
+	cin >> stNgaySinh;
+	cout << "nhap dia chi: ";
+	cin >> stDiaChi;
+}
+
+void CNhanVien::Xuat()
+{
+	cout << "ho ten: " << this->stHoTen << endl;
+	cout << "ngay sinh: " << this->stNgaySinh << endl;
+	cout << "dia chi: " << this->stDiaChi << endl;
+	cout << "luong la: " << this->fLuong << endl;
+}
+
+float CNhanVien::TinhLuong()
+{
+	return fLuong;
+}
